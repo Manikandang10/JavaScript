38a744ad83269d5a8217261a8789f2aa52b3e5be
Manikandan G <gmanikandan149@gmail.com>
2024-09-03 16:05:02 +0530

    Create Prompt

---
+++ Prompt.js
@@ -0,0 +1,6 @@
+var a=parseInt(prompt("Enter A value :"));
+var b=parseInt(prompt("Enter B value :"));
+var c=a+b;
+document.write("A value is :"+a+"<br>");
+document.write("B value is :"+b+"<br>");
+document.write("<b style=font-size:40px>Sum Of the Value A and B :"+c+"</b>");
