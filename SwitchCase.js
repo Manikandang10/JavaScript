48e18f68b8933e0cf3adb757d873069ba4873d68
Manikandan G <gmanikandan149@gmail.com>
2024-09-03 16:11:33 +0530

    Create SwitchCase

---
+++ SwitchCase.js
@@ -0,0 +1,38 @@
+
+var a=parseInt(prompt("Enter the digit :"));
+
+switch(a)
+{
+case 1:
+		document.write("One");
+		break;
+case 2:
+		document.write("Two");
+		break;
+case 3:
+		document.write("Three");
+		break;
+case 4:
+		document.write("Four");
+		break;
+case 5:
+		document.write("Five");
+		break;
+case 6:
+		document.write("Six");
+		break;
+case 7:
+		document.write("Seven");
+		break;
+case 8:
+		document.write("Eight");
+		break;
+case 9:
+		document.write("Nine");
+		break;
+case 0:
+		document.write("Zero");
+		break;
+default:
+	document.write("Enter Only Single digit....");
+}
